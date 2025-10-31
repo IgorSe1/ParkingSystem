7426ef1aebeb832ce232efc763a9a0204987fce5
IgorSe1 <many91248@gmail.com>
2025-10-31 13:29:46 +0200

    Delete Селищев_Ігор_КБ-31_Repo/SpringRepository directory

--- Селищев_Ігор_КБ-31_Repo/SpringRepository/target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO users (name, email) VALUES ('Подлісецький Данило', 'podlisetskyi@gmail.com');
-INSERT INTO users (name, email) VALUES ('Селищев Ігор', 'selyshchev@gmail.com');
-INSERT INTO users (name, email) VALUES ('Сидорук Констянтин', 'sydoruk@gmail.com');
