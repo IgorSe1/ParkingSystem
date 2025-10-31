ec8cccccf73976fc7d6700f194225be6afa3d3e3
IgorSe1 <many91248@gmail.com>
2025-10-31 13:28:57 +0200

    1

---
+++ Селищев_Ігор_КБ-31_Repo/SpringRepository/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (name, email) VALUES ('Подлісецький Данило', 'podlisetskyi@gmail.com');
+INSERT INTO users (name, email) VALUES ('Селищев Ігор', 'selyshchev@gmail.com');
+INSERT INTO users (name, email) VALUES ('Сидорук Констянтин', 'sydoruk@gmail.com');
